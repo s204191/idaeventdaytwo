0da63f6356d8e53f19c03d66e99bdd4e6ae3d023
Broberg <cbr@delegate.dk>
2022-10-03 15:10:33 +0200

    Get and set metadata fromCosmos and image resizer

--- AzureWebApiSolution/Controllers/DemoController.cs
+++ AzureWebApiSolution/Controllers/DemoController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Imaging;
+using Azure.Storage.Blobs;
+using System.Drawing;
+using Microsoft.Azure.Cosmos;
+using AzureWebApiSolution.Models;
 
 namespace AzureWebApiSolution.Controllers
 {    
@@ -7,9 +12,11 @@ namespace AzureWebApiSolution.Controllers
     public class DemoController : ControllerBase
     {
         private readonly ILogger<DemoController> _logger;
+        private readonly CosmosClient _cosmosClient;
         public DemoController(ILogger<DemoController> logger)
         {
             _logger = logger;
+            //_cosmosClient = new CosmosClient(Environment.GetEnvironmentVariable("CosmosConnection"));
         }
 
         [HttpGet]
@@ -28,7 +35,98 @@ namespace AzureWebApiSolution.Controllers
             return Ok($"Received {nr1} & {nr2} from http request. {nr1} + {nr2} = {nr1 + nr2} Timestamp: {DateTime.Now}");
         }
 
-        //Lav endpoint til at modtage billeder
-        //Gør klar til endpoint der skal sætte metadata 
+        [HttpPost]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        public async Task<IActionResult> ResizeImage(string fileName, IFormFile formFile)
+        {
+            try
+            {
+                //TODO: Fix mig
+                //var formdata = await req.ReadFormAsync();
+                //var file = req.Form.Files["file"];
+                var formcollection = await Request.ReadFormAsync();
+                var file = formcollection.Files.FirstOrDefault();
+
+
+                var image = Image.FromStream(file.OpenReadStream());
+                var resized = new Bitmap(image, new Size(256, 256));
+
+                using var imageStream = new MemoryStream();
+                resized.Save(imageStream, ImageFormat.Jpeg);
+                imageStream.Position = 0;
+
+                var blobService = new BlobServiceClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"));
+                var blobClient = new BlobContainerClient(Environment.GetEnvironmentVariable("AzureWebJobsStorage"), "Images");
+                var blob = blobClient.GetBlobClient(fileName);
+                await blob.UploadAsync(imageStream);
+
+                return Ok(file.FileName + " - " + file.Length.ToString() + " bytes");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        public async Task<IActionResult> SetMetaData(Metadata metadata)
+        {
+            try
+            {
+                //Get cosmos container
+                var db = _cosmosClient.GetDatabase("MetaData");
+                var containerLink = await db.CreateContainerIfNotExistsAsync(new ContainerProperties
+                {
+                    Id = "MyMetaData",
+                    PartitionKeyPath = "/id",
+                    DefaultTimeToLive = -1,
+
+                }, ThroughputProperties.CreateAutoscaleThroughput(1000));
+
+                //Create new object in Cosmos from request
+                var response = await containerLink.Container.UpsertItemAsync(metadata, new PartitionKey(metadata.Id));
+                
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(Metadata), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetMetaData(string billedeId)
+        {
+            try
+            {
+                //Get cosmos container
+                var db = _cosmosClient.GetDatabase("MetaData");
+                var containerLink = await db.CreateContainerIfNotExistsAsync(new ContainerProperties
+                {
+                    Id = "MyMetaData",
+                    PartitionKeyPath = "/id",
+                    DefaultTimeToLive = -1,
+
+                }, ThroughputProperties.CreateAutoscaleThroughput(1000));
+
+                //Get specifiedId (if existing)
+                var sqlQuery = $"SELECT * FROM c WHERE c.Id = '{billedeId}'"; // <-- Query to specify search
+
+                var result = containerLink.Container.GetItemQueryIterator<Metadata>(sqlQuery);
+                while (result.HasMoreResults)
+                {
+                    var message = await result.ReadNextAsync();
+                    return Ok(message.FirstOrDefault());                
+                }
+
+                return Ok(billedeId + " Something went wrong!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
     }
 }
---
+++ AzureWebApiSolution/Models/Metadata.cs
@@ -0,0 +1,10 @@
+﻿namespace AzureWebApiSolution.Models
+{
+    public class Metadata
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Id { get; set; }
+        public string Location { get; set; }
+    }
+}
