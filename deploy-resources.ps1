d2111916755154d298f26d01114563d11a636c96
Ramtin Asef-Nakhai <ran@delegate.dk>
2022-10-04 22:01:23 +0200

    typo...

--- deploy-resources.ps1
+++ deploy-resources.ps1
@@ -64,7 +64,7 @@ $storageAccountName = 'ramtinstorageacc'
 $appServicePlanName = 'ramtinAppServicePlan'
 $functionappName = 'ramtinFuncApp111'
 $webappName = 'ramtinWebApp111'
-$cosmosNamespace = 'ramtincosmosnameSpace'
+$cosmosNamespace = 'ramtincosmosnamespace'
 
 # If you don't have the latest az cli, then az functionapp step might fail.
 # If so, then please remove the # from the next line.
